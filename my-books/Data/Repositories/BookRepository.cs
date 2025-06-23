0e6cf35631fb5676dd7bbfd178d27d22f518b1f0
mohyeDev <mohye20@gmail.com>
2025-06-24 00:43:26 +0300

    implement complete BookRepository

---
+++ my-books/Data/Repositories/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books.Data.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task AddAsync(Book book)
+        {
+            _context.books.AddAsync(book);
+            return Task.CompletedTask;
+        }   
+
+        public Task DeleteAsync(Book book)
+        {
+              _context.books.Remove(book);
+            return _context.SaveChangesAsync();             
+        }
+
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.books.Include(b => b.Genre).ToListAsync();
+        }
+
+        public async Task<Book> GetByIdAsync(int Id)
+        {
+            return await _context.books.Include(b=>b.Genre).FirstOrDefaultAsync(b=>b.Id == Id); ;
+        }
+
+        public Task UpdateAsync(Book book)
+        {
+            _context.books.Update(book);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ my-books/Data/Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using my_books.Data.Models;
+
+namespace my_books.Data.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetByIdAsync(int Id);
+
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+
+        Task DeleteAsync(Book book);
+    }
+}
